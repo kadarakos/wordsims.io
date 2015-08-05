6c2c106806f7c470bdc7fe3a06c7ff839c87b1fa
kadar_akos <kadar.akos@gmail.com>
2015-08-05 09:34:22 +0200

    grid form on upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from wordsims.wordsims import WordSim
 
 @route('/')
 def root():
-    return static_file('index.html', root='.')
+    return static_file('test.html', root='.')
 
 @route('/upload', method='POST')
 def do_upload():
